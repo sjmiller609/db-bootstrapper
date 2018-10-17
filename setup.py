8f1d4d97df41d986fb69e2de1b0ae522a7cdaa27
schnie <greg@astronomer.io>
2018-10-17 16:46:05 -0400

    Pin urllib3 due to issue between kubernetes and requets libs

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         'SQLAlchemy==1.2.9',
         'sqlalchemy-utils==0.33.0',
         'kubernetes==7.0.0',
+        'urllib3==1.23',
     ],
     entry_points={
         'console_scripts': [
